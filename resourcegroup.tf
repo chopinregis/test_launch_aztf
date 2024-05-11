2f79cc10b43b7229d5233bb5bb4c09c7599ffd8e
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-10 20:07:24 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "mcit_resource_group_420-ZZ5-UM-Regis_Learning"
+  name     = var.myresourcegroupname
   location = "canadacentral"
 }
