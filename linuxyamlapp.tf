f6a909097ed26ec9ff2032b365f916c0b4dab810
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-10 20:52:36 -0400

    Update linuxyamlapp.tf

--- linuxyamlapp.tf
+++ linuxyamlapp.tf
@@ -1,5 +1,5 @@
 locals{
-  linux_app=[for f in fileset("${path.module}/regisconfigs", "[^_]*.yaml") : yamldecode(file("${path.module}/regisconfigs/${f}"))]
+  linux_app=[for f in fileset("${path.module}/${var.regisconfigs}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.regisconfigs}/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
       for linuxapps in try(app.listoflinuxapp, []) :{
