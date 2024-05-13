0250a2c94bc22961f17d57a2cb47e040f73db5e8
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-13 19:11:48 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,3 +16,35 @@ variable "myresourcegroupname" {
   type = string
   default = "mcit420zz5um"
 }
+
+
+
+
+
+# Variable declaration
+variable "regisconfigs" {
+  description = "Path to the configuration files"
+  type        = string
+  # Provide a default or expect the user to specify it
+}
+
+# Resource group (Example, make sure this matches your actual needs)
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  # Ensure to configure the required properties for AKS here
+}
+
+# Local value using 'regisconfigs'
+locals {
+  linux_app = [for f in fileset("${path.module}/${var.regisconfigs}", "[^_]*.yaml") :
+    yamldecode(file("${path.module}/${var.regisconfigs}/${f}"))]
+}
