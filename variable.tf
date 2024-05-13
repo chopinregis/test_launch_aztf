304c5f78bf37d45b1b7358f41dca48903d74da09
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-13 19:34:50 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -50,6 +50,17 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
 }
 
 
+resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
+  for_each = { for cluster in azurerm_kubernetes_cluster.batchabcd : cluster.name => cluster }
+
+  name                  = each.key
+  kubernetes_cluster_id = each.value.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+}
+
+
+
 
 variable "regisconfigs" {
   description = "Path to the configuration files"
